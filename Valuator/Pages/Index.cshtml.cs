ef57b97cffd33a089cc59bc2c45de0cf26df0e02
Barabashhka <pavlik.danilov.2003@mail.ru>
2024-04-28 01:55:03 +0300

    Сделал первую лабу

---
+++ Valuator/Pages/Index.cshtml.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace Valuator.Pages;
+
+public class IndexModel : RedisPageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger, ConnectionMultiplexer redis): base(redis)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+
+    public IActionResult OnPost(string text)
+    {
+        if (String.IsNullOrEmpty(text))
+        {
+            return Redirect("about");
+        }
+
+        
+        _logger.LogDebug(text);
+
+        string id = Guid.NewGuid().ToString();
+        
+        string similarityKey = "SIMILARITY-" + id;
+        RedisDatabase.StringSet(similarityKey, GetSimilarity(text));
+
+        string textKey = "TEXT-" + id;
+        RedisDatabase.StringSet(textKey, text);
+
+        string rankKey = "RANK-" + id;
+        RedisDatabase.StringSet(rankKey, GetRank(text));
+
+        return Redirect($"summary?id={id}");
+    }
+
+    private double GetRank(string text)
+    {
+        var notLetterCount = text.Count(ch => !char.IsLetter(ch));
+
+        return (double) notLetterCount / text.Length;
+    }
+
+    private int GetSimilarity(string text)
+    {
+        var keys = RedisServer.Keys();
+        var isSimilarText = keys.Any(key =>
+            key.ToString().Substring(0, 5) == "TEXT-" && RedisDatabase.StringGet(key) == text);
+
+        return  isSimilarText ? 1 : 0;
+    }
+}
---
+++ Valuator/Pages/Summary.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
+
+namespace Valuator.Pages;
+public class SummaryModel : RedisPageModel
+{
+    private readonly ILogger<SummaryModel> _logger;
+
+    public SummaryModel(ILogger<SummaryModel> logger, ConnectionMultiplexer redis): base(redis)
+    {
+        _logger = logger;
+    }
+
+    public double Rank { get; set; }
+    public double Similarity { get; set; }
+
+    public void OnGet(string id)
+    {
+        _logger.LogDebug(id);
+
+        string rankKey = "RANK-" + id;
+        Rank = (double)RedisDatabase.StringGet(rankKey);
+
+        string similarityKey = "SIMILARITY-" + id;
+        Similarity = (double)RedisDatabase.StringGet(similarityKey);
+    }
+}
